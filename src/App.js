33405bc0454eb3c0662854d2b99ddfb505111747
Gayathri <gayathrimca333@gmail.com>
2023-12-31 06:38:47 +0530

    weather-dashboard is working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import { useSelector } from 'react-redux';
-// import LoginPage from './LoginPage';
+import LoginPage from './LoginPage';
 import DashboardPage from './Pages/DashboardPage';
 
 
@@ -16,11 +16,11 @@ function App() {
   return (
     <div >
      <div>
-      {/* {isAuthenticated ? (
+      {isAuthenticated ? (
         <DashboardPage />
       ) : (
         <LoginPage />
-      )} */}
+      )}
       <DashboardPage />
      
       
